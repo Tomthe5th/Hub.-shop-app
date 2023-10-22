f708d79c7ddcc1f49aaf8f1cdeb528222b86ee46
Tom 「季林ユミル」 <swanhtet304y56@gmail.com>
2023-10-21 20:07:30 -0700

    #

---
+++ app/products/[slug]/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Productpage() {
+  return <div>Productpage</div>;
+}
--- components/productcard.jsx
+++ components/productcard.jsx
@@ -1,17 +1,49 @@
+"use client";
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { Expand, ShoppingCart } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 export default function Productcard() {
+  const router = useRouter();
+
+  function handleClick(event) {
+    router.push("/products/Name");
+  }
+function stopPropagation(e) {
+e.stopPropagation()
+}
+
   return (
-    <article className="max-w-lg"> 
-      <div className="aspect-square rounded-xl relative "> 
+    <article className="max-w-lg " onClick={handleClick}>
+      <div className="aspect-square rounded-xl relative group">
         <Image
           src="https://picsum.photos/200"
           alt="product label"
           fill
-          className="object-cover rounded-md"
+          className="object-cover aspect-square rounded-md"
         />
+        <div className="absolute top-5 right-5 flex items-center gap-4 text-white opacity-0 group-hover:opacity-100 " onClick={stopPropagation}>
+          <Button variant="ghost" size="18" className={cn("p-2")}>
+            <Expand />
+          </Button>
+          <Button variant="ghost" size="18" className={cn("p-2")}>
+            <ShoppingCart />
+          </Button>
+        </div>
+      </div>
+      <div className=" flex flex-col items-center justify-center">
+        <div>
+          <p>Name</p>
+          <p>categoryName</p>
+        </div>
+
+        <div>
+          <p>...$</p>
+        </div>
       </div>
     </article>
   );
