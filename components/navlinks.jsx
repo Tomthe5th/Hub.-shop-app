76f31316dd9f5d49dbbb5421f3ae64ecff3cc3cf
Tom 「季林ユミル」 <swanhtet304y56@gmail.com>
2023-11-15 01:40:52 +0630

    small ui changes

--- components/navlinks.jsx
+++ components/navlinks.jsx
@@ -4,6 +4,24 @@ import { Key } from "lucide-react";
 import Link from "next/link";
 import { useParams, usePathname, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+// import {
+//   NavigationMenu,
+//   NavigationMenuContent,
+//   NavigationMenuIndicator,
+//   NavigationMenuItem,
+//   NavigationMenuLink,
+//   NavigationMenuList,
+//   NavigationMenuTrigger,
+//   NavigationMenuViewport,
+// } from "@/components/ui/navigation-menu";
 
 export default function Navlinks({ data }) {
   const searchParams = useSearchParams();
@@ -22,18 +40,55 @@ export default function Navlinks({ data }) {
 
   return (
     <nav className="flex items-center gap-4 ; lg:gap-6 mx-6 ">
-      {links.map((link) => (
+      {links.slice(0, 3).map((link) => (
         <Link
           key={link.id}
           href={link.href}
           className={cn(
-            "test-sm font-medium hover:text-black hover:underline transition-color",
+            "navlink",
             link.isActive ? "text-black underline-offset-1" : "text-slate-500 "
           )}
         >
           {link.name}
         </Link>
       ))}
+      <DropdownMenu>
+        <DropdownMenuTrigger className={cn("navlink")}>
+          more{" "}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {links.slice(4).map((link) => (
+            <>
+              {" "}
+              <DropdownMenuItem key={link.id} className={cn("flex flex-row")}>
+                <Link
+                  href={link.href}
+                  className={cn(
+                    "navlink",
+                    link.isActive
+                      ? "text-black underline-offset-1"
+                      : "text-slate-500 "
+                  )}
+                >
+                  {link.name}
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+            </>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {/* 
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu> */}
     </nav>
   );
 }
--- components/productcard.jsx
+++ components/productcard.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Skeleton } from "./ui/skeleton";
 import Image from "next/image";
 import { Button } from "./ui/button";
@@ -23,12 +23,7 @@ export default function Productcard({ products }) {
   return (
     <article className="w-64 " onClick={handleClick}>
       <div className="aspect-square w-full  rounded-xl relative group">
-        <Image
-          fill
-          src={imageUrl}
-          alt="product label"
-          className="object-cover aspect-square rounded-md"
-        />
+        <ProductImage imageUrl={imageUrl} name={name} />
         <div
           className="absolute top-5 right-5 flex items-center gap-4 text-white opacity-0 group-hover:opacity-100 "
           onClick={stopPropagation}
@@ -53,5 +48,24 @@ export default function Productcard({ products }) {
       </div>
     </article>
   );
+
+  function ProductImage({ imageUrl, name = "" }) {
+    const [imageLoading, setImageloading] = useState(true);
+
+    return (
+      <Image
+        onLoadingComplete={() => setImageloading(false)}
+        fill
+        src={imageUrl}
+        alt={name}
+        className={cn(
+          "object-cover aspect-square rounded-md  ease-in-out duration-300",
+          imageLoading
+            ? "blur-md scale-110 grayscale "
+            : " scale-100 grayscale-0"
+        )}
+      />
+    );
+  }
 }
 // 1:27:30
